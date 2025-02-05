8049b3ab48297abfa9b41dbbf510b379576b119b
jborruel308@me.com <jborruel308@me.com>
2025-02-04 22:25:56 -0800

    hiddengem form html complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('hiddengem/create/', views.HiddenGemCreate.as_view(), name='hiddengem-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import HiddenGem
 
 
+class HiddenGemCreate(CreateView):
+    model = HiddenGem
+    fields = '__all__'
+    
 def home(request):
     return render(request, 'main_app/home.html')
 
