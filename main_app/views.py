2e7910f5f4106c1264c0422147db788b75f680b4
jborruel308@me.com <jborruel308@me.com>
2025-02-05 14:02:05 -0800

    switching branches to update maind models

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import HiddenGem
+from .forms import HiddenGemForm
+
 
 
 class HiddenGemCreate(CreateView):
     model = HiddenGem
-    fields = '__all__'
+    from_class = HiddenGemForm
+
     
 def home(request):
     return render(request, 'main_app/home.html')
