fad19e574333b7ae22c959d42ade4c84b7050a27
jborruel308@me.com <jborruel308@me.com>
2025-02-05 18:32:35 -0800

    superuser setup

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -27,6 +28,10 @@ class HiddenGem(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse()
+
     
 
 class Activty(models.Model):
