b1cbcc237b1bfead397d2cca868ad751e8898de3
jborruel308@me.com <jborruel308@me.com>
2025-02-09 10:49:17 -0800

    login page complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
 from .models import HiddenGem
 from .forms import HiddenGemForm
 
@@ -7,9 +8,14 @@ from .forms import HiddenGemForm
 
 class HiddenGemCreate(CreateView):
     model = HiddenGem
-    from_class = HiddenGemForm
+    form_class = HiddenGemForm
+
 
     
 def home(request):
     return render(request, 'main_app/home.html')
+
+
+class Login(LoginView):
+    template_name = 'registration/login.html'
 
