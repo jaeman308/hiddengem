ae30de27b1586896e8f6e600a9f9da977dce0d99
Astrid Ge <qiutongg@umich.edu>
2025-02-12 16:48:16 +0800

    complete index page,detial page need to add comment feature still

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from main_app.models import HiddenGem
+from main_app.models import HiddenGem, Activity
 from .forms import HiddenGemForm, CustomUser
 
 
@@ -47,7 +47,14 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'main_app/signup.html', context)
 
-        
+def hiddengem_index(request):
+    hiddengems = HiddenGem.objects.all()
+    return render(request, 'hidden_gem/index.html', {'hiddengems': hiddengems})
+
+def hiddengem_detial(request, hiddengem_id):
+    hiddengem = HiddenGem.objects.get(id=hiddengem_id)
+    return render(request, 'hidden_gem/detial.html', {'hiddengem': hiddengem})
+
 
 
 
